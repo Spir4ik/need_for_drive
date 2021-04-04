3624595a050f1da21c236a0b01de0dd7638625ae
Egor Furanin <sven2.cool@gmail.com>
2021-04-05 00:32:09 +0300

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 import MainPage from "./pages/MainPage.jsx";
 import CarReservation from "./pages/CarReservation.jsx";
 import ModelsPage from "./pages/ModelsPage.jsx";
+import AdditionalPage from './pages/AdditionalPage.jsx'
 import {Route, Switch} from "react-router";
-import {useSelector} from "react-redux";
 import "./styles/index.scss"
 
 export default function () {
-    const value = useSelector(state => state.order);
-    console.log(value);
     return(
         <Switch>
             <Route exact path="/" component={MainPage} />
             <Route path="/carreservation" component={CarReservation} />
             <Route path="/modelspage" component={ModelsPage} />
+            <Route path="/additional" component={AdditionalPage} />
         </Switch>
     )
 }
--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-import {createAction} from 'redux-actions'
-
-export const getCity = createAction("GET_CITY");
-export const getPoint = createAction("GET_POINT");
-export const getCar = createAction("GET_CAR");
